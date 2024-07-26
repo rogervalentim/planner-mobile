776647d47846d00f9532e1acfab377445317bf2e
rogervalentim <valentim.roger33@gmail.com>
2024-07-26 17:24:17 -0300

    feat: add input component

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,9 +1,34 @@
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
+import { MapPin, Calendar as IconCalendar } from "lucide-react-native";
+
+import { colors } from "@/styles/colors";
+
+import { Input } from "@/components/input";
 
 export default function Index() {
   return (
-    <View>
-      <Text>Hello world</Text>
+    <View className="flex-1 items-center justify-center px-5">
+      <Image
+        source={require("@/assets/logo.png")}
+        className="h-8"
+        resizeMode="contain"
+      />
+
+      <Text className="text-zinc-400 font-regular text-center text-lg mt-3">
+        Convide seus amigos e planeje sua{"\n"} próxima viagem
+      </Text>
+
+      <View className="w-full bg-zinc-900 p-4 rounded-xl my-8 border border-zinc-800">
+        <Input>
+          <MapPin color={colors.zinc[400]} size={20} />
+          <Input.Field placeholder="Para onde?" />
+        </Input>
+
+        <Input>
+          <IconCalendar color={colors.zinc[400]} size={20} />
+          <Input.Field placeholder="Para onde?" />
+        </Input>
+      </View>
     </View>
   );
 }
